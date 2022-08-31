286974a897d5358494de7354be1268a8557be79d
Camila <camila99pessoa@gmail.com>
2022-08-31 01:14:51 -0300

    creating controllers

---
+++ src/app/controllers/userController.js
@@ -0,0 +1,35 @@
+const userModel = require('../models/userModel');
+const checkToken = require('../middleware/auth');
+
+module.exports = app => {
+
+//----------------------Public routes-----------------------
+
+  app.post('/user/login', (req, res) => {
+    const user = req.body 
+    userModel.login(user, res)
+  })
+  
+  app.post('/user/signup', (req, res) => {
+    const user = req.body
+    userModel.registerUser(user, res)
+  })
+
+//----------------------Private routes----------------------
+
+  app.get('/user/:id', checkToken, async (req, res) => {
+    const id = parseInt(req.params.id)
+    userModel.getUserById(id, res)    
+  })
+
+  app.patch('/user/:id', checkToken, (req, res) => {
+      const id = parseInt(req.params.id)
+      const values = req.body 
+      userModel.updateUser(id, values, res)
+  })
+
+  app.delete('/user/:id', checkToken, (req, res) => {
+      const id = parseInt(req.params.id)
+      userModel.deleteUser(id, res)
+  }) 
+}
