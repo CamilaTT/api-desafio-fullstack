e7e64cc74ea8ea699e31b248af70f0bdd6252e78
Camila Fernandes <87024844+CamilaTT@users.noreply.github.com>
2022-10-28 20:12:48 -0300

    add request header middleware

--- src/app/controllers/vehicleController.js
+++ src/app/controllers/vehicleController.js
@@ -1,31 +1,29 @@
 const vehicleModel = require('../models/vehicleModel')
-const addRequestHeader = require('../middleware/requestHeader');
+const requestHeader = require('../middleware/requestHeader');
 
 module.exports = app => {
     
-    app.use(addRequestHeader);
-   
-    app.get('/vehicle', (req, res) => {
+    app.get('/vehicle', requestHeader, (req, res) => {
         vehicleModel.listVehicles(res)
     })
 
-    app.get('/vehicle/:id', (req, res) => {
+    app.get('/vehicle/:id', requestHeader, (req, res) => {
         const id = parseInt(req.params.id)
         vehicleModel.selectVehicleById(id, res)
     })
 
-    app.post('/vehicle', (req, res) => {
+    app.post('/vehicle', requestHeader, (req, res) => {
         const VEHICLE = req.body;
         vehicleModel.addVehicle(VEHICLE, res)
     })
 
-    app.put('/vehicle/:id', (req, res) => {
+    app.put('/vehicle/:id', requestHeader, (req, res) => {
         const id = parseInt(req.params.id)
         const values = req.body 
         vehicleModel.updateVehicle(id, values, res)
     })
 
-    app.delete('/vehicle/:id', (req, res) => {
+    app.delete('/vehicle/:id', requestHeader, (req, res) => {
         const id = parseInt(req.params.id)
         vehicleModel.deleteVehicle(id, res)
     }) 
