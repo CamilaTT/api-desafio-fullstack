3ac4f11d770f5eb58ff1794913167622e9c609e6
Camila Fernandes <87024844+CamilaTT@users.noreply.github.com>
2022-10-28 20:17:05 -0300

    Update vehicledataController.js

--- src/app/controllers/vehicledataController.js
+++ src/app/controllers/vehicledataController.js
@@ -1,11 +1,9 @@
 const vehicledataModel = require('../models/vehicledataModel')
-const addRequestHeader = require('../middleware/requestHeader');
+const requestHeader = require('../middleware/requestHeader');
 
 module.exports = app => {
-
-    app.use(addRequestHeader);
     
-    app.get('/vehicledata', (req, res) => {
+    app.get('/vehicledata', requestHeader, (req, res) => {
         let value = ''
         if(req.query.value) {
             value = req.query.value
@@ -13,23 +11,23 @@ module.exports = app => {
         vehicledataModel.listVehicleData(value, res)
     })
 
-    app.get('/vehicledata/:id', (req, res) => {
+    app.get('/vehicledata/:id', requestHeader, (req, res) => {
         const id = parseInt(req.params.id)
         vehicledataModel.getVehicledataById(id, res)
     })
 
-    app.post('/vehicledata', (req, res) => {
+    app.post('/vehicledata', requestHeader, (req, res) => {
         const VEHICLE_DATA = req.body;
         vehicledataModel.addVehicleData(VEHICLE_DATA, res)
     })
 
-    app.put('/vehicledata/:id', (req, res) => {
+    app.put('/vehicledata/:id', requestHeader, (req, res) => {
         const id = parseInt(req.params.id)
         const values = req.body 
         vehicledataModel.updateVehicleData(id, values, res)
     })
 
-    app.delete('/vehicledata/:id', (req, res) => {
+    app.delete('/vehicledata/:id', requestHeader, (req, res) => {
         const id = parseInt(req.params.id)
         vehicledataModel.deleteVehicleData(id, res)
     }) 
